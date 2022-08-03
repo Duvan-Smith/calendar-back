f14c69576072a216bd9e46cffb6645ecf9f03595
DsmithMr <dsmith.mr@gmail.com>
2022-08-02 21:48:22 -0500

    Added folder public

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({
-    ok: true,
-  });
-});
+app.use(express.static("public"));
+
+// TODO: Mover logica
+// app.get("/", (req, res) => {
+//   res.json({
+//     ok: true,
+//   });
+// });
 
 app.listen(4000, () => {
   console.log(`Puerto ${4000}`);
