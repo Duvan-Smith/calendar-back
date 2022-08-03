abb62a7e8c20cf27ddea8626af6d6bf7c584a708
DsmithMr <dsmith.mr@gmail.com>
2022-08-02 22:27:08 -0500

    Control de variables resividas

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,16 +8,31 @@ const revalidateToken = (req = request, res = response) => {
 };
 
 const loginUser = (req = request, res = response) => {
+  const { email, password } = req.body;
   res.json({
     ok: true,
     msg: "login",
+    email,
+    password,
   });
 };
 
 const createUser = (req = request, res = response) => {
+  const { name, email, password } = req.body;
+
+  if (name.length < 5) {
+    return res.status(400).json({
+      ok: false,
+      msg: "Tamaño del nombre incorrecto",
+    });
+  }
+
   res.json({
     ok: true,
     msg: "create",
+    name,
+    email,
+    password,
   });
 };
 
