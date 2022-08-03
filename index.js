61fa52ce7dbcafc303753c35ffc1bf4e5f71f869
DsmithMr <dsmith.mr@gmail.com>
2022-08-02 21:42:58 -0500

    Added get

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("first");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    ok: true,
+  });
+});
+
+app.listen(4000, () => {
+  console.log(`Puerto ${4000}`);
+});
