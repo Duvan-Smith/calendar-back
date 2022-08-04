42bab28490aefe765187f18e0d0413ef338eb7b3
DsmithMr <dsmith.mr@gmail.com>
2022-08-04 14:02:48 -0500

    connection mongoose

---
+++ database/config.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const dbConection = async () => {
+  try {
+    await mongoose.connect(process.env.DB_CNN);
+    console.log("DB online");
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error al iniciar la DB");
+  }
+};
+
+module.exports = {
+  dbConection,
+};
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const { dbConection } = require("./database/config");
 require("dotenv").config();
 
 const app = express();
+
+dbConection();
 
 app.use(express.static("public"));
 app.use(express.json());
