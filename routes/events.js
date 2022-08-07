1267c64706061880450e5fc81c0417d1791f697f
DsmithMr <dsmith.mr@gmail.com>
2022-08-07 11:57:15 -0500

    Added validation to Dates

--- routes/events.js
+++ routes/events.js
@@ -2,20 +2,32 @@
     host + /api/events
 */
 const { Router } = require("express");
+const { check } = require("express-validator");
 const { validarJWT } = require("../middlewares/validar-jwt");
+const { validarCampos } = require("../middlewares/validar-campos");
 const {
   getEvents,
   createEvent,
   updateEvent,
   deleteEvent,
 } = require("../controllers/events");
+const { isDate } = require("../helpers/isDate");
 
 const router = Router();
 
 router.use(validarJWT);
 
 router.get("/", getEvents);
-router.post("/", createEvent);
+router.post(
+  "/",
+  [
+    check("title", "El titulo es obligatorio").not().isEmpty(),
+    check("start", "La fecha inicio es obligatoria").custom(isDate),
+    check("end", "La fecha final es obligatoria").custom(isDate),
+    validarCampos,
+  ],
+  createEvent
+);
 router.put("/:id", updateEvent);
 router.delete("/:id", deleteEvent);
 
